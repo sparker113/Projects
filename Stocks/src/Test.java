6523a0714ce2f58953dc443cedb5e3306e8d6ec4
sparker113 <85771862+sparker113@users.noreply.github.com>
2023-06-28 11:35:50 -0500

    Updated graphpanel
    Added PlotFrame class to graphpanel package; added functionality to AxisPanel to make the x-axis dates

--- Stocks/src/Test.java
+++ Stocks/src/Test.java
@@ -1,5 +1,17 @@
+import java.time.LocalDate;
+import java.util.Map;
+
+import data.Nasdaq;
+
 public class Test {
-    public static void main(String[] ars){
-        
-    }
+	public static void main(String[] args) throws Exception{
+		Nasdaq nasdaq = Nasdaq.getAllStockData();
+		nasdaq.getData();
+		nasdaq.setHistoricData(30);
+		Map<String,Map<LocalDate,Map<String,String>>> map = nasdaq.getHistoricMap();
+		System.out.println(map.size());
+		System.out.println(nasdaq.getData().size());
+	}
+	
+	
 }
